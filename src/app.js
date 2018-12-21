b216f37777e2722cccbc4fee1a6c328fce40e457
Vlada <n.vladyslava@gmail.com>
2018-12-21 21:49:56 +0200

    sorted into components

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
 
-const template = <p>THIS IS JSX FROM WEBPACK</p>;
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
 
-ReactDOM.render(template, document.getElementById('app'));
+
